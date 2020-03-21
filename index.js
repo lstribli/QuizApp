2c356e398eaf7aaa67f14475008cc443848ce406
Logan Stribling <logan.stribling@gmail.com>
2020-03-21 13:25:28 -0600

    needs debugging

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 'use strict';
 
 function refresh() {
-    startQuiz()
-    generateQuestionsElement();
-    //handleAnswerClick();
-    // handleAnswerCheck();
-    // handleToNextPage();
-  }
-  
-  // when the page loads, call `refresh`
-  $(refresh);
+  startQuiz();
+  generateQuiz();
+  renderStart();
+  showStart();
+  startPage();
+  handleStart();
+  startQuiz();
+  restartHandler();
+  generateQuestionsElement();
+  renderQuestion();
+  newAnswer();
+  handleNewAnswer();
+  formEventHandler();
+  renderFinalWindow();
+  generateQuestionsElement();
+}
+
+//handleAnswerClick();
+// handleAnswerCheck();
+// handleToNextPage();
+
+
+// when the page loads, call `refresh`
+$(refresh);
--- renderQuestionsPage.js
+++ renderQuestionsPage.js
@@ -1,18 +1,51 @@
-"use strict";
-//check the state of quizstarted
-//return either questions or welcome page
-//store.questions
-//store.questionNumber
-//map over answers array
+'use strict';
+function renderStart() {
+  $('<js-start-page>').html(
+    `
+  <div class="js-start-quiz"><button>start</button></div>`;
+}
+  )
+function showStart() {
+  $('js-start-page').show();
+}
+function startPage() {
+  $('js-final-page').hide();
+  $('js-question-page').hide();
 
+}
+
+
+function handleStart() {
+  $('js-start-quiz').click(() => {
+    $('js-question-page').show();
+    $('js-start-page').hide();
+    $('js-final-page').hide();
+    startQuiz();
+  });
+}
+
+function restartHandler() {
+  $('.js-restart').click(event => {
+    $('.js-final-page').hide();
+    startQuiz();
+  });
+}
 
 function startQuiz() {
-  
- 
+
+  STORE.questionNumber = 0;
+  STORE.score = 0;
+  renderQuestion();
+  $('js-notification').text('The quiz has started');
+  $('js-question-page').show();
+  $('main').html(generateQuestionsElement());
+
+}
+
 
 function generateQuestionsElement() {
-
   return `
+  <main>
     <form class= "formOne">
         <fieldset>
     
@@ -20,41 +53,87 @@ function generateQuestionsElement() {
         
             <input type="radio" name="answer" value="${STORE.questions[STORE.questionNumber].answers[0]}" required>
             <label for="answer">${STORE.questions[STORE.questionNumber].answers[0]}</label><br/>
-
             <input type="radio" name="answer" value="${STORE.questions[STORE.questionNumber].answers[1]}">
             <label for="answer">${STORE.questions[STORE.questionNumber].answers[1]}</label><br/>
-
             <input type="radio" name="answer" value="${STORE.questions[STORE.questionNumber].answers[2]}">
             <label for="answer">${STORE.questions[STORE.questionNumber].answers[2]}</label><br/>
-
             <input type="radio" name="answer" value="${STORE.questions[STORE.questionNumber].answers[3]}">
             <label for="answer">${STORE.questions[STORE.questionNumber].answers[3]}</label><br/>
         </fieldset>
         <button class="submit" type="submit">Submit</button>
     </form>
+    </main>
     `;
 }
 
+function renderQuestion() {
+  const questionAnswers = generateQuestionsElement(STORE.questions);
+  $('#formOne').html(questionAnswers);
+}
 
-
-//log value of submitted radio button
-function formEventHandler() {
-  $('main').on('submit', 'form', event => {
+function answer() {
+  $('submit').on('click', event => {
     event.preventDefault();
-    console.log(event.target.answer.value);
+    return event.target.answer.value;
   });
+
+
+
+
+  function handleNewAnswer(answer) {
+    if (!answer) {
+      $('js-notification').text('correct answer');
+    }
+    return;
+  }
+  if (answer === STORE.questions[STORE.questionNumber].correctAnswer) {
+    STORE.score++;
+  }
+  STORE.questionNumber++;
+
+  if (STORE.questionNumber >= STORE.questions.length) {
+    $('.js-final-page').show();
+  } else {
+    renderQuestion();
+  }
+
+
+  function renderFinalWindow() {
+    $('js-question-page').hide();
+    $('js-final-page').show();
+    $('js-final-message').text('quiz is over your score is ${STORE.score}');
+  }
+
+  //log value of submitted radio button
+  function formEventHandler() {
+    $('main').on('submit', 'form', event => {
+      event.preventDefault();
+      handleNewAnswer(event.target.answer.value);
+    });
+  }
+
+  function renderQuestion() {
+    const questionAnswers = generateQuestionsElement(STORE);
+    $('#formOne').html(questionAnswers);
+  }
+
+
+  function generateQuiz() {
+    renderStart();
+    showStart();
+    startPage();
+    handleStart();
+    startQuiz();
+    restartHandler();
+    generateQuestionsElement();
+    renderQuestion();
+    answer();
+    handleNewAnswer();
+    formEventHandler();
+    renderFinalWindow();
+  }
+
+  $(generateQuiz);
+
+
 }
-
-function switchQuestion() {
-for (let i=0; i<questions.length; i++) { correctAnswers=[]; for (letter in questions[i].answers){ answers.push(${STORE.QUESTION})
-}
-//make sure answer is correct
-
-//if correct increment score
-//if incorrect
-
-//increment
-
-
-
-
